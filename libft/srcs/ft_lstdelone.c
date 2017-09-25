0233338b88255b0592390f6f11fcc4097b736fdc
agouby <agouby42@gmail.com>
2017-09-25 16:53:09 +0200

    freeing mlx at errors points. Need to do numbers and minus coordinates

--- libft/srcs/ft_lstdelone.c
+++ libft/srcs/ft_lstdelone.c
@@ -14,10 +14,8 @@
 
 void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t))
 {
-	t_list	*temp;
 
-	temp = *alst;
 	del(temp->content, temp->content_size);
-	free(temp);
+	free(*alst);
 	*alst = NULL;
 }
